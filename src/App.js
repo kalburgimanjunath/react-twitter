75dbfdbc7313e5932b0a233fd7aa909ab9f88346
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-26 22:23:03 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -11,10 +11,25 @@ import {
   Lists,
   Messages,
   Profile,
+  Settings,
 } from './pages/index.js';
+import Modal from 'react-modal';
 export default function App() {
   const [users, setUsers] = useState();
   const [posts, setPosts] = useState();
+  const [modalIsOpen, setIsOpen] = useState(false);
+  let subtitle;
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = 'blue';
+  }
   const getApiData = async () => {
     const response = await fetch(
       'https://jsonplaceholder.typicode.com/users'
@@ -38,6 +53,25 @@ export default function App() {
     <div>
       <Switch>
         <Route path="/home">
+          <button onClick={openModal}>Open Modal</button>
+          <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            // style={customStyles}
+            contentLabel="Example Modal"
+          >
+            <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+            <button onClick={closeModal}>close</button>
+            <div>I am a modal</div>
+            <form>
+              <input />
+              <button>tab navigation</button>
+              <button>stays</button>
+              <button>inside</button>
+              <button>the modal</button>
+            </form>
+          </Modal>
           <Dashboard users={users} posts={posts} />
         </Route>
         <Route path="/dashboard">
@@ -60,6 +94,9 @@ export default function App() {
         </Route>
         <Route path="/bookmarks">
           <Bookmarks users={users} posts={posts} />
+        </Route>
+        <Route path="/settings">
+          <Settings users={users} posts={posts} />
         </Route>
         <Route path="/*" exact>
           <Login />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,77 +5,125 @@ export default function Login() {
   const loginForm = [{ username: 'text', password: 'password' }];
   return (
     <div>
-      <h1>Login</h1>
-      <div>
-        <Formik
-          initialValues={{
-            email: 'manjunathkalburgi@gmail.com',
-            password: 'Password@123',
-          }}
-          validate={(values) => {
-            const errors = {};
-            if (!values.email) {
-              errors.email = 'Required';
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = 'Invalid email address';
-            }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-            location.href = './dashboard';
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            /* and other goodies */
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <input
-                type="email"
-                name="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-              />
-              <br />
-
-              <input
-                type="password"
-                name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-              />
-              <div>
-                <label className="error">
-                  {errors.email && touched.email && errors.email}
-                </label>
-                <label className="error">
-                  {errors.password && touched.password && errors.password}
-                </label>
+      <div className="container">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 col-md-offset-4">
+              <div className="panel panel-default">
+                <div className="panel-heading">
+                  <h3 className="panel-title">Login via site</h3>
+                </div>
+                <div className="panel-body">
+                  <Formik
+                    initialValues={{
+                      email: 'manjunathkalburgi@gmail.com',
+                      password: 'Password@123',
+                    }}
+                    validate={(values) => {
+                      const errors = {};
+                      if (!values.email) {
+                        errors.email = 'Required';
+                      } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                          values.email
+                        )
+                      ) {
+                        errors.email = 'Invalid email address';
+                      }
+                      return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                      setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                      }, 400);
+                      location.href = './dashboard';
+                    }}
+                  >
+                    {({
+                      values,
+                      errors,
+                      touched,
+                      handleChange,
+                      handleBlur,
+                      handleSubmit,
+                      isSubmitting,
+                    }) => (
+                      <form
+                        accept-charset="UTF-8"
+                        role="form"
+                        onSubmit={handleSubmit}
+                      >
+                        <div>
+                          <label className="error">
+                            {errors.email && touched.email && errors.email}
+                          </label>
+                          <label className="error">
+                            {errors.password &&
+                              touched.password &&
+                              errors.password}
+                          </label>
+                        </div>
+                        <fieldset>
+                          <div className="form-group">
+                            <input
+                              className="form-control"
+                              type="email"
+                              name="email"
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.email}
+                            />
+                          </div>
+                          <div className="form-group">
+                            <input
+                              className="form-control"
+                              placeholder="Password"
+                              name="password"
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.password}
+                            />
+                          </div>
+                          <div className="checkbox">
+                            <label>
+                              <input
+                                name="remember"
+                                type="checkbox"
+                                value="Remember Me"
+                              />
+                              Remember Me
+                            </label>
+                          </div>
+                          <input
+                            className="btn btn-lg btn-success btn-block"
+                            type="submit"
+                            value="Login"
+                            disabled={isSubmitting}
+                          />
+                        </fieldset>
+                      </form>
+                    )}
+                  </Formik>
+                  <hr />
+                  <center>
+                    <h4>OR</h4>
+                  </center>
+                  <input
+                    className="btn btn-lg btn-success btn-facebook btn-block"
+                    type="submit"
+                    value="Login via facebook"
+                  />
+                  <input
+                    className="btn btn-lg btn-success btn-facebook btn-block"
+                    type="submit"
+                    value="Login via Google"
+                  />
+                </div>
               </div>
-              <button
-                type="submit"
-                className="btn btn-primary"
-                disabled={isSubmitting}
-              >
-                Submit
-              </button>
-            </form>
-          )}
-        </Formik>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
